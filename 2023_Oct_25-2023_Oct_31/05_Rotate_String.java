ad6e8bdefd295f85691d782a76e360eb1762d038
Jesus Morales <53502658+jgmorale@users.noreply.github.com>
2023-10-28 09:24:44 -0600

    Rotate string problem

---
+++ 2023_Oct_25-2023_Oct_31/05_Rotate_String.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/rotate-string/
+
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (goal.length() != s.length()) return false;
+
+        Character first = goal.charAt(0);
+        int n = s.length();
+        int start = -1;
+
+        for(int i = 0; i < n; i++){
+            if (start != -1 && goal.charAt(i-start) != s.charAt(i)) start = -1;
+            if (s.charAt(i) == first && start == -1) start = i;
+        }
+
+        if(start != -1){
+            for(int i = 0; i < start; i++){
+                if(s.charAt(i) != goal.charAt(n-start+i)) return false;
+            }
+        }
+
+        return start != -1 ? true : false;
+    }
+}
