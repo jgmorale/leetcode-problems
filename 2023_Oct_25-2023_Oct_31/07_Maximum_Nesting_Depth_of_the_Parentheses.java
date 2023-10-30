3404710753971381b1646c9f728945a74644a138
Jesus Morales <53502658+jgmorale@users.noreply.github.com>
2023-10-29 22:35:20 -0600

    Maximum nesting depth problem

--- 2023_Oct_25-2023_Oct_31/07_Maximum_Nesting_Depth_of_the_Parentheses.java
+++ 2023_Oct_25-2023_Oct_31/07_Maximum_Nesting_Depth_of_the_Parentheses.java
@@ -1,8 +1,15 @@
 // https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
-import java.util.*;
 
 class Solution {
     public int maxDepth(String s) {
-        
+        int currDeep = 0;
+        int maxDeep = 0;
+        for(int i = 0; i < s.length(); i++){
+            Character curr = s.charAt(i);
+            if (curr == '(') currDeep++;
+            if (curr == ')') currDeep--;
+            if (currDeep > maxDeep) maxDeep = currDeep;
+        }   
+        return maxDeep;
     }
 }
