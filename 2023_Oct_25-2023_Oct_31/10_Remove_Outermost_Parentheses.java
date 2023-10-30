c7ccbef78d92d38f747c03b3089e6ff6fc2a0592
Jesus Morales <53502658+jgmorale@users.noreply.github.com>
2023-10-30 00:46:18 -0600

    Start of remove outermost parentheses problem

---
+++ 2023_Oct_25-2023_Oct_31/10_Remove_Outermost_Parentheses.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/remove-outermost-parentheses/
+class Solution {
+    public String removeOuterParentheses(String s) {
+        if(s.length() == 1) return s; // trivial case
+        int i = 0, j = 0, k = 0, count = 0;
+        String output = "";
+        while(i < j && j < s.length()){
+            
+            i++;
+        }
+        return output;
+    }
+}
