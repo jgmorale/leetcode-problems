d14a4963e050b85817325ae72d3c92bf063d2314
Jesus Morales <53502658+jgmorale@users.noreply.github.com>
2023-10-29 23:12:05 -0600

    Add sort character by frequency problem

--- 2023_Oct_25-2023_Oct_31/08_Sort_Character_by_Frequency.java
+++ 2023_Oct_25-2023_Oct_31/08_Sort_Character_by_Frequency.java
@@ -3,18 +3,32 @@ import java.util.*;
 
 class Solution {
     public String frequencySort(String s) {
-        HashMap<Character, Integer> letterCount = new HashMap<Character, Integer>();
-        
+        if (s.length() == 1) return s;
+        HashMap<Character,String> group = new HashMap<Character, String>();
         for(int i = 0; i < s.length(); i++){
-            Character curr = s.charAt(i);
-            if(!letterCount.containsKey(curr){
-                letterCount.put(curr, 1);
+            Character current = s.charAt(i);
+            if (!group.containsKey(current)){
+                group.put(current, String.valueOf(current));
             } else {
-                letterCount.put(curr, letterCount.get(curr) + 1);
+                group.put(current, group.get(current) + current);
             }
         }
 
-        
+        List<String> gr = new ArrayList<String>(group.values());
+        gr.sort(new StringLengthComparator());
 
+        String output = "";
+        for(int i = 0; i < gr.size(); i++){
+            output += gr.get(i);
+        }
+
+        return output;
+    }
+
+    private class StringLengthComparator implements Comparator<String> {
+        @Override
+        public int compare(String s1, String s2) {
+            return s2.length() - s1.length();
+        }
     }
 }
