8d9731fb41bf8d6fb7b0ab6096f28d31a94938af
Jesus Morales <53502658+jgmorale@users.noreply.github.com>
2023-12-11 20:53:24 -0600

    Fix typo in trapped water

--- 08_2023_Dic_06_2023_Dic_12/02_Trapping_Rain_Water.java
+++ 08_2023_Dic_06_2023_Dic_12/02_Trapping_Rain_Water.java
@@ -1,12 +1,12 @@
 // https://leetcode.com/problems/trapping-rain-water/
+
 import java.util.*;
 
-// Still working on a working solution
 class Solution {
     public int trap(int[] height) {
         // [0,1,0,2,1,0,1,3,2,1,2,1]
 
-        int num = height.length;, trappedWater = 0, numElem = 0, min = 0, lastMax = 0;
+        int num = height.length, trappedWater = 0, numElem = 0, min = 0, lastMax = 0;
         Stack<Integer> stack = new Stack<Integer>();
         for(int i = 0; i < num; i++){
             while(!stack.isEmpty() && height[i] > lastMax){
